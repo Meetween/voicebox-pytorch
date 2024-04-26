5d0f8bb86f9c970e5a066afcbd0869a198642238
pier-maker92 <pierfrancesco.melucci@gmail.com>
2024-04-26 18:04:40 +0000

    ipa tokenizer

--- voicebox_pytorch/config.py
+++ voicebox_pytorch/config.py
@@ -1,3 +1,6 @@
+from tokenizer.arpa_to_ipa import arpa_to_ipa_lookup
+
+
 def dict_to_object(src):
     class DictToObject:
         def __init__(self, dictionary):
@@ -12,6 +15,7 @@ def dict_to_object(src):
     return DictToObject(src)
 
 
+ipa_phonemes = list(arpa_to_ipa_lookup.values())
 config = dict_to_object(
     {
         # Audio
@@ -38,89 +42,22 @@ config = dict_to_object(
         "tokenizer_style": {"pitch_min": -2, "pitch_max": 2, "tokens": 256},
         # Tokenizer
         "tokenizer": {
-            "vocab_size": 4096,
+            "vocab_size": 5 + len(ipa_phonemes),
             "vocab_output": [
                 "<PAD>",
                 "<BOS>",
                 "<EOS>",
                 "<SIL>",
                 "<UNK>",
-                "AA0",
-                "AA1",
-                "AA2",
-                "AE0",
-                "AE1",
-                "AE2",
-                "AH0",
-                "AH1",
-                "AH2",
-                "AO0",
-                "AO1",
-                "AO2",
-                "AW0",
-                "AW1",
-                "AW2",
-                "AY0",
-                "AY1",
-                "AY2",
-                "B",
-                "CH",
-                "D",
-                "DH",
-                "EH0",
-                "EH1",
-                "EH2",
-                "ER0",
-                "ER1",
-                "ER2",
-                "EY0",
-                "EY1",
-                "EY2",
-                "F",
-                "G",
-                "HH",
-                "IH0",
-                "IH1",
-                "IH2",
-                "IY0",
-                "IY1",
-                "IY2",
-                "JH",
-                "K",
-                "L",
-                "M",
-                "N",
-                "NG",
-                "OW0",
-                "OW1",
-                "OW2",
-                "OY0",
-                "OY1",
-                "OY2",
-                "P",
-                "R",
-                "S",
-                "SH",
-                "T",
-                "TH",
-                "UH0",
-                "UH1",
-                "UH2",
-                "UW0",
-                "UW1",
-                "UW2",
-                "V",
-                "W",
-                "Y",
-                "Z",
-                "ZH",
-            ],
+            ]
+            + ipa_phonemes,
             # Special tokens
             "pad_token": "<PAD>",
             "silence_token": "<SIL>",
             "sequence_begin_token": "<BOS>",
             "sequence_end_token": "<EOS>",
             "unknown_token": "<UNK>",
+            "special_tokens": ["<PAD>", "<SIL>", "<BOS>", "<EOS>", "<UNK>"],
         },
     }
 )
--- voicebox_pytorch/tokenizer/tokenizer.py
+++ voicebox_pytorch/tokenizer/tokenizer.py
@@ -1,10 +1,11 @@
-import torch
-import sentencepiece as spm
+from typing import List
+from .arpa_to_ipa import arpa_to_ipa_lookup
 
 
 class Tokenizer:
     def __init__(self, config):
         self.vocab_size = config.tokenizer.vocab_size
+        self.arpa_to_ipa_lookup = arpa_to_ipa_lookup
 
         # Tokens
         self.pad_token = config.tokenizer.pad_token
@@ -12,6 +13,7 @@ class Tokenizer:
         self.sequence_begin_token = config.tokenizer.sequence_begin_token
         self.sequence_end_token = config.tokenizer.sequence_end_token
         self.unknown_token = config.tokenizer.unknown_token
+        self.special_tokens = config.tokenizer.special_tokens
 
         # Phoneme map
         self.phoneme_to_id = {}
@@ -26,9 +28,23 @@ class Tokenizer:
         self.sequence_end_token_id = self.phoneme_to_id[self.sequence_end_token]
         self.unknown_token_id = self.phoneme_to_id[self.unknown_token]
 
-    def encode_phonemes(self, phonemes):
+    def arpa_to_ipa(self, arpa_phonemes: List[str]):
+        ipa_phonemes = []
+        for p in arpa_phonemes:
+            if p not in self.special_tokens:
+                try:
+                    ipa_phonemes.append(self.arpa_to_ipa_lookup[p])
+                except KeyError:
+                    raise ValueError(f"Phoneme {p} not found in the arpa_to_ipa_lookup")
+            else:  # special tokens handling
+                ipa_phonemes.append(p)
+        return ipa_phonemes
+
+    def encode_phonemes(self, phonemes, convert_to_ipa=True):
         return (
-            [self.sequence_begin_token_id] + [self.phoneme_to_id[p] for p in phonemes] + [self.sequence_end_token_id]
+            [self.sequence_begin_token_id] + [self.phoneme_to_id[p] for p in self.arpa_to_ipa(phonemes)]
+            if convert_to_ipa
+            else [self.phoneme_to_id[p] for p in phonemes] + [self.sequence_end_token_id]
         )
 
     def decode_phonemes(self, phonemes):
