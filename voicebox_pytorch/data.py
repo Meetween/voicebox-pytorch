ee1438acbd5d592c3a07365bc2923a0c5901fe72
pier-maker92 <pierfrancesco.melucci@gmail.com>
2024-04-26 18:05:21 +0000

    LibriTTS-R integration

--- voicebox_pytorch/data.py
+++ voicebox_pytorch/data.py
@@ -1,22 +1,20 @@
+import os
 import json
+import math
+import textgrid
+from tqdm import tqdm
 from glob import glob
 from pathlib import Path
 from functools import wraps
-
 from einops import rearrange
-from tqdm import tqdm
-
 from beartype import beartype
 from beartype.door import is_bearable
 from beartype.typing import Optional, Tuple, Union
 
 import torch
+import torchaudio
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset, DataLoader
-
-import torchaudio
-
-# utilities
 
 
 def exists(val):
@@ -28,31 +26,37 @@ def cast_tuple(val, length=1):
 
 
 # dataset functions
-def parse_lab_file(filename, sampling_rate=75.0):
-    labels = []
+def align_phoneme(labels, downsample_factor, sr=24000):
+    sampling_rate = sr / downsample_factor
     silence_tokens = ["sp", "sil", "spn", ""]
-    with open(filename, "r") as file:
-        for line in file:
-            parts = line.split("\t")  # Strip and split on tabs
-
-            start_time = float(parts[0])
-            end_time = float(parts[1])
-            label = parts[2].split("\n")[0]
-
-            # Replace 'sp' with 'SIL' or add 'SIL' if the label is empty
-            if label in silence_tokens:
-                label = "<SIL>"
-
-            labels.append((start_time, end_time, label))
-
     # Generate the sequence of labels based on duration and sampling rate
     label_sequence = []
-    for start_time, end_time, label in labels:
+    residual = 0
+    for label, start_time, end_time in labels:
+        if label in silence_tokens:
+            label = "<SIL>"
         duration_in_seconds = end_time - start_time
         repeat_count = int(duration_in_seconds * sampling_rate)
+        residual += duration_in_seconds * sampling_rate - repeat_count
+        if residual > 1:
+            repeat_count += 1
+            residual -= 1
         label_sequence.extend([label] * repeat_count)
-
+    end_total_time = math.ceil(end_time * sampling_rate)
+    if len(label_sequence) < end_total_time - 2:
+        label_sequence.extend(["<SIL>"] * ((end_total_time - 2) - len(label_sequence)))
+    if len(label_sequence) >= end_total_time - 2:
+        label_sequence = label_sequence[: end_total_time - 2]
+    assert len(label_sequence) == end_total_time - 2, f"{len(label_sequence)} != {end_total_time - 2}"
     return label_sequence
+
+
+def parse_textgrid(file_path):
+    tg = textgrid.TextGrid.fromFile(file_path)
+    phonemes_with_timestamp = []
+    for item in tg[1]:
+        phonemes_with_timestamp.append((item.mark if item.mark else "<SIL>", item.minTime, item.maxTime))
+    return phonemes_with_timestamp
 
 
 class AudioDataset(Dataset):
@@ -61,59 +65,62 @@ class AudioDataset(Dataset):
         self,
         folder,
         audio_extension=".wav",
-        phoneme_extension=".lab",
+        phoneme_extension=".TextGrid",
         json_pathlist=None,
         tokenizer=None,
         reuturn_text=False,
+        downsample_factor=None,
     ):
         super().__init__()
         path = Path(folder)
         assert path.exists(), "folder does not exist"
         assert exists(tokenizer), "tokenizer must be provided"
+        assert exists(json_pathlist), "json_pathlist must be provided"
+        assert downsample_factor is not None, "downsample_factor must be provided"
 
-        self.audio_extension = audio_extension
-        phone_files = glob(f"{folder}/**/**/*{phoneme_extension}")
-        print(phone_files[0])
-        if not exists(json_pathlist):
-            audio_files = [f.replace(phoneme_extension, audio_extension) for f in phone_files]
-        else:
-            with open(json_pathlist, "r") as f:
-                pathlist = json.load(f)
-            audio_files = [v["path"] for v in pathlist.values()]
-            # intersect the two lists
-            phone_files_names = [f.split(".")[0] for f in phone_files]
-            audio_files = [f.split(".")[0] for f in audio_files]
-            valid_files_names = set(phone_files_names) & set(audio_files)
-            phone_files = [f + phoneme_extension for f in valid_files_names]
-            audio_files = [f + audio_extension for f in valid_files_names]
-
-        assert len(audio_files) > 0, "no audio_files found"
-        assert len(audio_files) == len(phone_files), "audio_files and phone_files must have the same length"
-        print(f"found {len(audio_files)} audio files and {len(phone_files)} phone files")
-
-        audio_files.sort()
-        phone_files.sort()
-
-        self.data = list(zip(audio_files, phone_files))
+        with open(json_pathlist, "r") as f:
+            valid_files_per_split = json.load(f)
+        # read file names for each split
+        self.data = [
+            {
+                "audio": os.path.join(
+                    folder,
+                    split,
+                    name.split("_")[0],
+                    name.split("_")[1],
+                    name + audio_extension,
+                ),
+                "textgrid": os.path.join(
+                    folder,
+                    split,
+                    name.split("_")[0],
+                    name.split("_")[1],
+                    name + phoneme_extension,
+                ),
+            }
+            for split, file_names in valid_files_per_split.items()
+            for name in file_names
+        ]
 
         self.tokenizer = tokenizer
         self.return_text = reuturn_text
+        self.downsample_factor = downsample_factor
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        audio_file, phone_file = self.data[idx]
-        phonemes = parse_lab_file(phone_file)
+        data = self.data[idx]
+        phonemes = align_phoneme(parse_textgrid(data["textgrid"]), self.downsample_factor)
         phoneme_ids = self.tokenizer.encode_phonemes(phonemes)
 
-        wave, sr = torchaudio.load(audio_file)
+        wave, sr = torchaudio.load(data["audio"])
         assert sr == 24_000, "sample rate must be 24_000"
         wave = rearrange(wave, "1 ... -> ...")
 
         data = {"wave": wave, "phoneme_ids": torch.tensor(phoneme_ids)}
         if self.return_text:
-            text_file = phone_file.replace(".lab", ".original.txt")
+            text_file = data["textgrid"].replace(".TextGrid", ".original.txt")
             with open(text_file, "r") as f:
                 text = f.read()
             data["text"] = text
--- voicebox_pytorch/preprocess_data.py
+++ voicebox_pytorch/preprocess_data.py
@@ -6,56 +6,27 @@ import argparse
 import librosa
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser(description="Preprocess the dataset")
-parser.add_argument(
-    "--data_dir", type=str, default="$SCRATCH/data/LibriTTS/train-clean-360", help="path to the dataset"
-)
-parser.add_argument(
-    "--phonemes_dir",
-    type=str,
-    default="$PLG_GROUPS_STORAGE/plggmeetween/meetween_datasets/LibriTTSCorpusLabel/lab/phone/train-clean-360",
-    help="path to the integrated phoneme labels",
-)
-
 """This script takes the folder of the original dataset and integrates phoneme labels information, providing an hdf5 file"""
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    SCRATCH = os.environ.get("SCRATCH")
-    PLG_GROUPS_STORAGE = os.environ.get("PLG_GROUPS_STORAGE")
-    data_dir = args.data_dir.replace("$SCRATCH", SCRATCH)
-    phonemes_dir = args.phonemes_dir.replace("$PLG_GROUPS_STORAGE", PLG_GROUPS_STORAGE)
-    assert os.path.exists(data_dir), f"Data directory {data_dir} does not exist"
-    assert os.path.exists(phonemes_dir), f"Phonemes directory {phonemes_dir} does not exist"
-
+    with open("valid_files.json", "r") as f:
+        valid_files = json.load(f)
     # create a json file with the audio file that are below 5s lenghts
-
-    audio_files = glob(os.path.join(data_dir, "**", "**", "*.wav"))
-    print(f"Found {len(audio_files)} audio files")
+    root = "/home/ubuntu/data/LibriTTSR/LibriTTS_R"
     valid_audio_files = {}
-    for audio_file in tqdm(audio_files):
-        duration = librosa.get_duration(filename=audio_file)
-        if duration < 8:
-            phone_file = audio_file.replace(".wav", ".lab")
-            if not os.path.exists(phone_file):
-                print(f"Phoneme file {phone_file} does not exist")
+    for split, files in valid_files.items():
+        valid_audio_files[split] = []
+        print(f"Split: {split} - {len(files)} files")
+        for file in tqdm(files, desc=f"Processing {split} files"):
+            speaker, folder = file.split("_")[:2]
+            try:
+                audio_file = os.path.join(root, split, speaker, folder, f"{file}.wav")
+                duration = librosa.get_duration(filename=audio_file)
+            except:
                 continue
-            file_name = audio_file.replace(".wav", "").replace(data_dir, "")
-            valid_audio_files[file_name] = {"duration": duration, "path": audio_file}
-    print(f"Found {len(valid_audio_files)} audio files below 8s")
-    with open("valid_audio_files.json", "w") as f:
-        json.dump(valid_audio_files, f)
 
-    # # Iterate over the dataset and integrate the phoneme labels
-    # lab_files = glob(os.path.join(phonemes_dir, "**", "**", "*.lab"))
-    # print(f"Found {len(lab_files)} lab files")
-    # for lab_file in tqdm(lab_files):
-    #     # Get the corresponding wav file
-    #     dest_lab_file = lab_file.replace(
-    #         f"{PLG_GROUPS_STORAGE}/plggmeetween/meetween_datasets/LibriTTSCorpusLabel/lab/phone/train-clean-360/",
-    #         f"{SCRATCH}/data/LibriTTS/train-clean-360/",
-    #     )
-    #     wav_file = dest_lab_file.replace(".lab", ".wav")
-    #     if os.path.exists(wav_file):
-    #         # Copy the lab file to the same directory as the wav file
-    #         shutil.copy(lab_file, dest_lab_file)
+            if duration <= 10:
+                valid_audio_files[split].append(file)
+
+    with open("valid_short_files.json", "w") as f:
+        json.dump(valid_audio_files, f)
